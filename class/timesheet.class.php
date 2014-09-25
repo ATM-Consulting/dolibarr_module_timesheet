0d122fd03d87365c9942da95570be68df772c59f
geoffrey.girard <geoffre.girard@atm-consulting.fr>
2014-09-25 17:47:02 +0200

    debug + création tache

--- class/timesheet.class.php
+++ class/timesheet.class.php
@@ -68,51 +68,98 @@ class TTimesheet extends TObjetStd {
 	
 	function savetimevalues(&$PDOdb,$Tab){
 		global $db,$user;
-
+		
 		//Parcours des tâches existantes pour MAJ temps
 		foreach($Tab as $cle => $value){
-			
+
 			if(is_array($value)){
 				
 				foreach($value as $idTask => $TTemps){
-					
+
 					$task = new Task($db);
 					$task->fetch($idTask);
 
-					foreach($TTemps as $date=>$temps){
+					if($idTask != 0){
 						
-						if($temps != ''){
-							$sql = "SELECT rowid FROM ".MAIN_DB_PREFIX."projet_task_time WHERE fk_task = ".$idTask." AND fk_user = ".$Tab['userid_'.$idTask]." AND task_date = '".$date."' LIMIT 1";
-							$PDOdb->Execute($sql);
+						$this->_updatetimespent($PDOdb,$Tab,$TTemps,$task,$idTask);
 
-							$timespent_duration_temp = explode(':',$temps);
-							$timespent_duration_temp = convertTime2Seconds((int)$timespent_duration_temp[0],(int)$timespent_duration_temp[1]);
+					}
+					else{
 
-							//Un temps a déjà été saisi pour ce projet, cette tache et cet utilisateur
-							if($PDOdb->Get_line()){
-
-								$task->fetchTimeSpent($PDOdb->Get_field('rowid'));
-
-								$task->timespent_duration = $timespent_duration_temp;
-								$task->timespent_fk_user = $Tab['userid_'.$idTask];
-
-								$task->updateTimeSpent($user);
-							}
-							//Un temps pour ce projet, cette tache et cet utilisateur n'existe pas encore, il faut l'ajouter
-							else{
-	
-								$task->timespent_date = $date;
-								$task->timespent_duration = $timespent_duration_temp;
-								$task->timespent_fk_user = $Tab['userid_'.$idTask];
-
-								$task->addTimeSpent($user);
-							}
-						}
+						$this->_addTask($PDOdb,$Tab,$TTemps,$idTask);
 					}
 				}
 			}
 		}
+		exit;
 		
+	}
+	
+	function _updatetimespent(&$PDOdb,&$Tab,&$TTemps,&$task,$idTask){
+		global $db, $user;
 		
+		foreach($TTemps as $date=>$temps){
+							
+			if($temps != ''){
+				$sql = "SELECT rowid FROM ".MAIN_DB_PREFIX."projet_task_time WHERE fk_task = ".$idTask." AND fk_user = ".$Tab['userid_'.$idTask]." AND task_date = '".$date."' LIMIT 1";
+				$PDOdb->Execute($sql);
+
+				$timespent_duration_temp = explode(':',$temps);
+				$timespent_duration_temp = convertTime2Seconds((int)$timespent_duration_temp[0],(int)$timespent_duration_temp[1]);
+
+				//Un temps a déjà été saisi pour ce projet, cette tache et cet utilisateur
+				if($PDOdb->Get_line()){
+
+					$task->fetchTimeSpent($PDOdb->Get_field('rowid'));
+
+					$task->timespent_duration = $timespent_duration_temp;
+					$task->timespent_fk_user = $Tab['userid_'.$idTask];
+
+					$task->updateTimeSpent($user);
+				}
+				//Un temps pour ce projet, cette tache et cet utilisateur n'existe pas encore, il faut l'ajouter
+				else{
+
+					$task->timespent_date = $date;
+					$task->timespent_duration = $timespent_duration_temp;
+					$task->timespent_fk_user = $Tab['userid_'.$idTask];
+
+					$task->addTimeSpent($user);
+				}
+			}
+		}
+	}
+	
+	function _addtask(&$PDOdb,&$Tab,&$TTemps,$idTask){
+		global $db,$user;
+
+		$product = new Product($db);
+
+		if($product->fetch($Tab['serviceid_0'])){
+			
+			$task = new Task($db);
+			$task->label = $product->label;
+
+			$defaultref='';
+			$obj = empty($conf->global->PROJECT_TASK_ADDON)?'mod_task_simple':$conf->global->PROJECT_TASK_ADDON;
+			if (! empty($conf->global->PROJECT_TASK_ADDON) && is_readable(DOL_DOCUMENT_ROOT ."/core/modules/project/task/".$conf->global->PROJECT_TASK_ADDON.".php"))
+			{
+				require_once DOL_DOCUMENT_ROOT ."/core/modules/project/task/".$conf->global->PROJECT_TASK_ADDON.'.php';
+				$modTask = new $obj;
+				$defaultref = $modTask->getNextValue($this->societe,$task);
+			}
+			
+			$task->ref = $defaultref;
+			$task->fk_project = $this->project->id;
+			$task->description = '';
+			$task->date_start = $this->date_deb;
+			$task->date_end = $this->date_fin;
+			$task->fk_task_parent = 0;
+
+			$idTask = $task->create($user);
+			//echo $idTask;exit;
+
+			_updatetimespent($PDOdb,$Tab,$TTemps,$idTask);
+		}
 	}
 }
