40b94fe3b4a1a2f8cbc87c6d20025b2cf2931e1e
root <root@tqmg01.tqm-insight.com>
2015-03-31 11:12:28 +0200

    FIX url quand port 4430

--- timesheet.php
+++ timesheet.php
@@ -208,9 +208,9 @@ function _liste() {
 		)
 		,'subQuery'=>array()
 		,'link'=>array(
-			'fk_societe'=>'<a href="'.dol_buildpath('/societe/soc.php?socid=@fk_societe@',2).'">'.img_picto('','object_company.png','',0).' @nom@</a>'
-			,'fk_project'=>'<a href="'.dol_buildpath('/projet/fiche.php?id=@fk_project@',2).'">'.img_picto('','object_project.png','',0).' @ref@</a>'
-			,'rowid'=>'<a href="'.dol_buildpath('/timesheet/timesheet.php?id=@rowid@',2).'">'.img_picto('','object_calendar.png','',0).' @rowid@</a>'
+			'fk_societe'=>'<a href="'.dol_buildpath('/societe/soc.php?socid=@fk_societe@',1).'">'.img_picto('','object_company.png','',0).' @nom@</a>'
+			,'fk_project'=>'<a href="'.dol_buildpath('/projet/fiche.php?id=@fk_project@',1).'">'.img_picto('','object_project.png','',0).' @ref@</a>'
+			,'rowid'=>'<a href="'.dol_buildpath('/timesheet/timesheet.php?id=@rowid@',1).'">'.img_picto('','object_calendar.png','',0).' @rowid@</a>'
 		)
 		,'translate'=>array(
 			'status'=>$TTimesheet->TStatus		
